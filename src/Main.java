dad914ac2d77704b1db902b851a1001cef996e17
Alex Baskakau <lenovos6000v4@gmail.com>
2023-02-24 13:17:58 +0000

    Code added

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        int ticketPrice = 29;
+        int ratio = 20;
+
+        System.out.println("Начислено миль- " + (ticketPrice / ratio));
+    }
+}
+
+
